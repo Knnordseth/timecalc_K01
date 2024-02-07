55da7d1408837148315b315c2bac9d29ebdc38b5
NotTidal <b.brandsegglein@gmail.com>
2024-02-07 13:19:41 +0100

    timecacl first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const hourStartInp = document.querySelector("#hourStart")
+const minStartInp = document.querySelector("#minStart")
+const secStartInp = document.querySelector("#secStart")
+const hourEndInp = document.querySelector("#hourStart")
+const minEndInp = document.querySelector("#minStart")
+const secEndInp = document.querySelector("#secStart")
+const calcBtn = document.querySelector("#calculate")
+
+const utskriftEl = document.querySelector("#utskrift")
+
+const sec = 1
+const min = 60 * sec
+const hour = 60 * min
+
+calcBtn.onclick = () => {
+    const hourStart = hourStartInp.value;
+    
+}
+
+
