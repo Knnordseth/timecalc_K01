94fc74924bbee1d160a95ae5b63eba3138d95d75
NotTidal <b.brandsegglein@gmail.com>
2024-02-08 10:21:18 +0100

    Working version calculator version 1

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const hourStartInp = document.querySelector("#hourStart")
 const minStartInp = document.querySelector("#minStart")
 const secStartInp = document.querySelector("#secStart")
-const hourEndInp = document.querySelector("#hourStart")
-const minEndInp = document.querySelector("#minStart")
-const secEndInp = document.querySelector("#secStart")
+const hourEndInp = document.querySelector("#hourEnd")
+const minEndInp = document.querySelector("#minEnd")
+const secEndInp = document.querySelector("#secEnd")
 const calcBtn = document.querySelector("#calculate")
 
 const utskriftEl = document.querySelector("#utskrift")
 
-const sec = 1
-const min = 60 * sec
-const hour = 60 * min
 
 calcBtn.onclick = () => {
-    const hourStart = hourStartInp.value;
-    
-}
+    const hourStart = parseInt(hourStartInp.value) || 0;
+    const minStart = parseInt(minStartInp.value) || 0;
+    const secStart = parseInt(secStartInp.value) || 0;
+
+    const hourEnd = parseInt(hourEndInp.value) || 0;
+    const minEnd = parseInt(minEndInp.value) || 0;
+    const secEnd = parseInt(secEndInp.value) || 0;
+
+    let totalTimeSeconds = (hourEnd * 3600 + minEnd * 60 + secEnd) - (hourStart * 3600 + minStart * 60 + secStart);
+    if (totalTimeSeconds < 0) {
+        totalTimeSeconds += 24 * 3600;
+
+    }
+
+    const hours = Math.floor(totalTimeSeconds / 3600);
+    totalTimeSeconds %= 3600;
+    const minutes = Math.floor(totalTimeSeconds / 60);
+    const seconds = totalTimeSeconds % 60;
+
+    utskriftEl.innerHTML = "";
+    utskriftEl.innerHTML = (`Total time: ${hours} Hours, ${minutes} Minutes, ${seconds} Seconds.`);
+
+
+};
 
 
